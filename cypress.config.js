6685c77991496757fafe5b7b186d0f8b8e9cc37e
Saif0922 <mohammedsaif0922@gmail.com>
2025-10-13 10:40:51 +0530

    test3

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    retries: {
+      runMode: 2,
+      openMode: 1
+    },
+    defaultCommandTimeout: 10000,
   },
 });
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,11 @@
 describe('Login Form', () => {
+  it('logs in with correct credentials', () => {
+    cy.visit('http://127.0.0.1:5500/public/index.html') // URL depends on your local server
+    cy.get('#username').type('admin')
+    cy.get('#password').type('1234')
+    cy.get('#loginButton').click()
+    cy.contains('Welcome, admin!').should('exist')
+  });
   it.only('logs in with correct credentials', () => {
     cy.visit('http://localhost:8080') // URL depends on your local server
     cy.get('#username').type('admin')
